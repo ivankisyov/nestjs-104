f6f8397b25eec28085efe4053157e7cad8acd680
Ivan Kisyov <dev.vanks@gmail.com>
2024-03-22 09:31:55 +0200

    Add more users service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,4 +14,31 @@ export class UsersService {
     const user = this.usersRepository.create({ email, password });
     return this.usersRepository.save(user);
   }
+
+  findOne(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.usersRepository.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.usersRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return this.usersRepository.remove(user);
+  }
 }
